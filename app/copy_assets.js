3e40897782553e0866468008a1846ec2ed32702b
purocean <purocean@gmail.com>
2019-08-07 09:41:18 +0800

    优化资源拷贝

--- app/copy_assets.js
+++ app/copy_assets.js
@@ -1,13 +1,18 @@
 const os = require('os')
+const path = require('path')
 const fs = require('fs-extra')
 
 const platform = os.platform()
 
-fs.copySync('src/assets', 'dist/assets')
-fs.copySync('static', 'dist/static')
+try {
+  fs.unlinkSync(path.join(__dirname, 'dist/assets'))
+} catch (error) {
+}
+
+fs.copySync(path.join(__dirname, 'src/assets'), path.join(__dirname, 'dist/assets'))
 
 if (platform === 'win32') {
-  fs.copySync('bin/pandoc/2.7.3/win32/pandoc.exe', 'dist/pandoc.exe')
+  fs.copySync(path.join(__dirname, 'bin/pandoc/2.7.3/win32/pandoc.exe'), path.join(__dirname, 'dist/pandoc.exe'))
 } else {
-  fs.copySync(`bin/pandoc/2.7.3/${platform}/pandoc`, 'dist/pandoc')
+  fs.copySync(path.join(__dirname, `bin/pandoc/2.7.3/${platform}/pandoc`), path.join(__dirname, 'dist/pandoc'))
 }
