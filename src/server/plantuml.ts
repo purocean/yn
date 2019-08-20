fab809dc4c7bfac9ce7421294ce8b7ae00952a8f
purocean <purocean@gmail.com>
2019-08-20 15:53:41 +0800

    修复 plantuml 中文乱码问题

--- src/server/plantuml.ts
+++ src/server/plantuml.ts
@@ -6,7 +6,7 @@ const generate = (data: any) => {
   const api = config.get('plantuml-api', 'local')
 
   if (api === 'local') {
-    const gen = plantuml.generate(data, {format: 'png'});
+    const gen = plantuml.generate(data, {format: 'png', charset: 'UTF-8'});
     return gen.out
   } else {
     return request(api.replace('{data}', encodeURIComponent(data)))
