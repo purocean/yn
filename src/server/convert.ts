0071375fb506fe37af7ed9b7c1617436174d4c47
purocean <purocean@gmail.com>
2019-11-20 13:17:17 +0800

    导出word使用模板文件

--- src/server/convert.ts
+++ src/server/convert.ts
@@ -2,17 +2,19 @@ import * as os from 'os'
 import * as fs from 'fs'
 import * as path from 'path'
 import { spawn } from 'child_process'
-import { BIN_DIR } from './constant'
+import { BIN_DIR, ASSETS_DIR } from './constant'
 
 const binPath = path.join(BIN_DIR, os.platform() + '-pandoc-2.7.3' + (os.platform() === 'win32' ? '.exe' : ''))
+const docxTplPath = path.join(ASSETS_DIR, './tpl.docx')
 
 const convert = async (html: string, type: string) => {
-  console.log(binPath)
   try {
     const path = os.tmpdir() + `/yn_convert_${new Date().getTime()}.${type}`
 
     return new Promise((resolve, reject) => {
-      const process = spawn(binPath, ['-f', 'html', '-o', path])
+      const args =  ['-f', 'html', '-o', path, '--reference-doc', docxTplPath]
+      console.log(binPath, args)
+      const process = spawn(binPath, args)
 
       process.on('close', () => {
         try {
