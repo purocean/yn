71264a50c7fdf58c4d6dbd49fa10628f984d9702
purocean <purocean@gmail.com>
2019-11-29 17:03:38 +0800

    增加tab组件

---
+++ frontend/src/components/FileTabs.vue
@@ -0,0 +1,34 @@
+<template>
+  <Tabs :list="list" v-model="current" @remove="removeTab"></Tabs>
+</template>
+
+<script>
+import Tabs from './Tabs'
+
+export default {
+  name: 'file-tabs',
+  components: { Tabs },
+  data () {
+    return {
+      list: [
+        { key: 'test1', label: 'TEST1', description: 'TTEESSTT2' },
+        { key: 'test2', label: 'TEST2', description: 'TTEESSTT3' },
+        { key: 'test3', label: 'TEST3', description: 'TTEESSTT4' },
+        { key: 'test4', label: 'TEST4', description: 'TTEESSTT5' },
+      ],
+      current: 'test1'
+    }
+  },
+  methods: {
+    removeTab (item) {
+      const list = this.list.filter(x => x.key !== item.key)
+      if (list.length > 0) {
+        setTimeout(() => {
+          this.current = list[list.length - 1].key
+        }, 50)
+      }
+      this.list = list
+    }
+  },
+}
+</script>
---
+++ frontend/src/components/Tabs.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="tabs">
+    <div
+      v-for="item in list"
+      :key="item.key"
+      :class="{tab: true, current: item.key === value}"
+      :title="item.description"
+      @click="chooseTab(item)">
+      <div class="label">{{item.label}}</div>
+      <div class="close" @click="removeTab(item)">
+        <y-icon class="close-icon" name="times" title="关闭"></y-icon>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import 'vue-awesome/icons/times'
+
+export default {
+  name: 'tabs',
+  props: {
+    value: String,
+    list: Array,
+  },
+  methods: {
+    chooseTab (item) {
+      this.$emit('input', item.key)
+    },
+    removeTab (item) {
+      this.$emit('remove', item)
+    }
+  },
+}
+</script>
+
+<style scoped>
+.tabs {
+  flex: none;
+  height: 30px;
+  background: #333;
+  display: flex;
+}
+
+.tab {
+  width: 100%;
+  max-width: 150px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-right: 1px;
+  color: #888;
+  cursor: default;
+  font-size: 12px;
+  background: #282828;
+}
+
+.label {
+  padding: 5px;
+}
+
+.tab.current {
+  color: #eee;
+  background: #1d1f21;
+}
+
+.close {
+  color: #999;
+  height: 18px;
+  width: 18px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50%;
+  margin-right: 5px;
+}
+
+.close:hover {
+  color: #aaa;
+  background: #444;
+}
+
+.close-icon {
+  zoom: .6;
+}
+</style>
