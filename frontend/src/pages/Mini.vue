8b88b20cd6054ed1e356228d7f91db0fed80d964
purocean <purocean@gmail.com>
2019-12-13 16:16:03 +0800

    移除代码

--- frontend/src/pages/Mini.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <div>迷你窗口</div>
-</template>
--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Main from './pages/Main'
-import Mini from './pages/Mini'
 
 Vue.use(Router)
 
@@ -9,6 +8,5 @@ export default new Router({
   mode: 'history',
   routes: [
     { path: '/', name: 'main', component: Main },
-    { path: '/mini', name: 'mini', component: Mini }
   ]
 })
