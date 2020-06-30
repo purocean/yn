83bc379832fa9705b822c9e948a4b2d9fd1f82c0
purocean <purocean@gmail.com>
2020-06-30 16:09:58 +0800

    升级 electron 到 9.0.5

--- win-install.bat
+++ win-install.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal
 set npm_config_disturl=https://atom.io/download/electron
-set npm_config_target=7.1.1
+set npm_config_target=9.0.5
 set npm_config_runtime="electron"
 set npm_config_cache=~\.npm-electron
 yarn && yarn run download-pandoc
