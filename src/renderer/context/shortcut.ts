98542f242336177d729081c6727cd274b39c008a
purocean <purocean@gmail.com>
2021-07-15 12:05:58 +0800

    fix: 修复输入法状态按回车问题

--- src/renderer/context/shortcut.ts
+++ src/renderer/context/shortcut.ts
@@ -13,7 +13,6 @@ export const LeftClick = 0
 type XKey = typeof Ctrl | typeof CtrlCmd | typeof Alt | typeof Shift
 
 const defaultActions = {
-  'show-quick-open': [CtrlCmd, 'p'],
   'file-tabs-switch-left': [Ctrl, Alt, 'ArrowLeft'],
   'file-tabs-switch-right': [Ctrl, Alt, 'ArrowRight'],
 }
