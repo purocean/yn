c514a0b7a524144295e34f8b095dec826ff9f4b7
purocean <purocean@gmail.com>
2021-12-20 23:15:03 +0800

    upd: update `ctx.base.readFromClipboard`

--- src/renderer/services/base.ts
+++ src/renderer/services/base.ts
@@ -105,20 +105,34 @@ export function getRepo (name: string) {
   return (getSetting('repos') || []).find(x => x.name === name)
 }
 
-export async function readFromClipboard (callback: (type: string, getType: (type: string) => Promise<Blob>) => Promise<void>) {
-  const result = await navigator.permissions.query({ name: 'clipboard-read' as any })
+export async function readFromClipboard (): Promise<Record<string, any>>
+export async function readFromClipboard (callback: (type: string, getType: (type: string) => Promise<Blob>) => Promise<void>): Promise<void>
+export async function readFromClipboard (callback?: (type: string, getType: (type: string) => Promise<Blob>) => Promise<void>): Promise<void | Record<string, any>> {
+  const permissionResult = await navigator.permissions.query({ name: 'clipboard-read' as any })
 
-  if (result.state === 'denied') {
+  if (permissionResult.state === 'denied') {
     useToast().show('warning', t('need-clipboard-permission'))
     return
   }
 
   const items: any = await (navigator.clipboard as any).read()
+
+  const result: Record<string, any> = {}
   for (const item of items) {
     for (const type of (item.types as string[])) {
-      await callback(type, item.getType.bind(item))
+      if (callback) {
+        await callback(type, item.getType.bind(item))
+      } else {
+        result[type] = await item.getType(type)
+      }
     }
   }
+
+  if (callback) {
+    return
+  }
+
+  return result
 }
 
 export async function writeToClipboard (type: string, value: any) {
