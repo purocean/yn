fac879f4b7f14410191aa7f5ad81373c062a79f1
purocean <purocean@gmail.com>
2022-01-05 11:39:22 +0800

    feat: add `ctx.utils.downloadDataURL` method

--- src/renderer/utils/index.ts
+++ src/renderer/utils/index.ts
@@ -89,6 +89,19 @@ export function downloadContent (filename: string, content: Buffer | string, typ
   }, 20000)
 }
 
+export function downloadDataURL (filename: string, dataURL: string) {
+  const byteString = atob(dataURL.split(',')[1])
+  const mimeString = dataURL.split(',')[0].split(':')[1].split(';')[0]
+  const ab = new ArrayBuffer(byteString.length)
+  const ia = new Uint8Array(ab)
+
+  for (let i = 0; i < byteString.length; i++) {
+    ia[i] = byteString.charCodeAt(i)
+  }
+
+  downloadContent(filename, ab, mimeString)
+}
+
 export function md5 (content: any) {
   return CryptoJS.MD5(content).toString()
 }
