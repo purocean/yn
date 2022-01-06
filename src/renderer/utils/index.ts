744f8f9c85faa3203d2c25ac3231b71a7450563b
purocean <purocean@gmail.com>
2022-01-06 23:17:31 +0800

    feat: add `ctx.utils.dataURLtoBlob` method

--- src/renderer/utils/index.ts
+++ src/renderer/utils/index.ts
@@ -23,6 +23,19 @@ export function encodeMarkdownLink (path: string) {
     .replace(/ /g, '%20')
 }
 
+export function dataURLtoBlob (dataURL: string) {
+  const byteString = atob(dataURL.split(',')[1])
+  const mimeString = dataURL.split(',')[0].split(':')[1].split(';')[0]
+  const ab = new ArrayBuffer(byteString.length)
+  const ia = new Uint8Array(ab)
+
+  for (let i = 0; i < byteString.length; i++) {
+    ia[i] = byteString.charCodeAt(i)
+  }
+
+  return new Blob([ab], { type: mimeString })
+}
+
 export function fileToBase64URL (file: File | Blob) {
   return new Promise<string>((resolve, reject) => {
     const fr = new FileReader()
