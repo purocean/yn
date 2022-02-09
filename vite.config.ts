7d2b9b47aa99f964828fa585d015cbd96d60039a
purocean <purocean@gmail.com>
2022-02-09 22:18:41 +0800

    feat: support jsx

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 import path from 'path'
 import fs from 'fs-extra'
 
@@ -52,7 +53,7 @@ if (!fs.existsSync(drawioDist)) {
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), vueJsx()],
   root: 'src/renderer',
   define: {
     __APP_VERSION__: JSON.stringify(process.env.npm_package_version)
