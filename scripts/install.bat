f259c2c3c160d0398aa025234ca7cb198161775f
purocean <purocean@gmail.com>
2022-09-22 07:34:04 +0800

    chore: update electron disturl

--- scripts/install.bat
+++ scripts/install.bat
@@ -1,4 +1,4 @@
-set npm_config_disturl=https://atom.io/download/electron
+set npm_config_disturl=https://electronjs.org/headers
 set npm_config_target=15.4.1
 set npm_config_runtime="electron"
 
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export npm_config_disturl=https://atom.io/download/electron
+export npm_config_disturl=https://electronjs.org/headers
 export npm_config_target=15.4.1
 export npm_config_runtime="electron"
 
