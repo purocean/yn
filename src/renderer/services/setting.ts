06a2d6bf6db92ab3a7573494ee161d9a5528f566
purocean <purocean@gmail.com>
2023-01-31 16:50:51 +0800

    feat(setting): limit repository name

--- src/renderer/services/setting.ts
+++ src/renderer/services/setting.ts
@@ -69,6 +69,7 @@ const schema: Schema = {
             title: 'T_setting-panel.schema.repos.name',
             defaultValue: '',
             maxLength: 10,
+            pattern: '^[^_\\s]+$',
             options: {
               inputAttributes: { placeholder: 'T_setting-panel.schema.repos.name-placeholder' }
             },
