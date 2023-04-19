c6bed225a7150c4bb9e02000835c4f2127dec115
purocean <purocean@gmail.com>
2023-04-19 14:31:37 +0800

    feat: premium v2

--- src/renderer/others/premium.ts
+++ src/renderer/others/premium.ts
@@ -1,101 +1,230 @@
-import CryptoJS from 'crypto-js'
-import dayjs from 'dayjs'
+import { LicenseToken } from 'app-license'
+import { PREMIUM_PUBLIC_KEY } from '@share/misc'
 import { getActionHandler } from '@fe/core/action'
-import { decrypt } from '@fe/utils/crypto'
+import * as api from '@fe/support/api'
 import { getSetting, setSetting } from '@fe/services/setting'
-import { getServerTimestamp } from '@fe/services/base'
 import { refresh } from '@fe/services/view'
-import { FLAG_DEMO, MODE } from '@fe/support/args'
+import { FLAG_DEMO, FLAG_MAS, MODE } from '@fe/support/args'
 import ga from '@fe/support/ga'
+import { getLogger, md5 } from '@fe/utils'
+import { registerHook } from '@fe/core/hook'
+import { useToast } from '@fe/support/ui/toast'
+import type { PremiumTab } from '@fe/types'
 
-interface License {
-  name: string,
-  email: string,
-  hash: string,
-  activateExpires: number,
-  expires: number,
+const tokenPrefix = 'ynkv2:'
+const logger = getLogger('premium')
+
+let licenseToken: LicenseToken | null | undefined
+let upgradeTryCount = 0
+
+type Payload = {
+  fetchToken: { licenseId: string }
+  removeDevice: { licenseId: string, device: string }
+  addDevice: { licenseId: string }
+  fetchDevices: { licenseId: string }
+  upgradeLicense: { oldLicense: string }
 }
 
-let purchased: boolean | null = null
+export async function requestApi<T extends keyof Payload> (method: T, payload: Payload[T]) {
+  const { data } = await api.fetchHttp('/api/premium', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ method, payload }),
+  })
 
-function parseLicense (licenseStr: string) {
-  const error = new Error('Invalid License')
+  return data
+}
 
-  if (!licenseStr) {
-    return null
+async function fetchToken (licenseId: string) {
+  const token = await requestApi('fetchToken', { licenseId })
+  return new LicenseToken(token, { publicOrPrivateKey: PREMIUM_PUBLIC_KEY })
+}
+
+function syncCacheLicenseToken (token: LicenseToken | null) {
+  logger.debug('syncCacheLicenseToken', !!token)
+  const oldToken = licenseToken
+  licenseToken = token
+
+  if (oldToken?.status !== token?.status) {
+    refresh()
   }
 
+  return licenseToken
+}
+
+export function tokenAvailableDays (token: LicenseToken) {
+  return Math.floor((token.expires.getTime() - Date.now()) / 1000 / 60 / 60 / 24)
+}
+
+export function tokenIsExpiredSoon (token: LicenseToken) {
+  if (FLAG_MAS) {
+    return false
+  }
+
+  return tokenAvailableDays(token) <= 3
+}
+
+async function upgradeV1License (oldLicense: string) {
+  logger.debug('upgradeV1License', oldLicense)
+  if (licenseToken) {
+    return
+  }
+
+  if (upgradeTryCount >= 1) {
+    return
+  }
+
+  upgradeTryCount++
+
   try {
-    licenseStr = [...licenseStr].reverse().join('')
-    licenseStr = CryptoJS.enc.Hex.parse(licenseStr).toString(CryptoJS.enc.Utf8)
-
-    if (licenseStr.length < 40) {
-      return null
-    }
-
-    const { content } = decrypt(
-      licenseStr.substring(32),
-      licenseStr.substring(0, 32)
-    )
-
-    const leading = 'yank-note-license'
-    if (content.startsWith(leading)) {
-      return JSON.parse(content.replace(leading, '')) as License
-    } else {
-      throw error
-    }
-  } catch {
-    throw error
+    const licenseId = await requestApi('upgradeLicense', { oldLicense })
+    await activateLicense(licenseId)
+    useToast().show('info', 'License upgraded successfully')
+    showPremium('activation')
+  } catch (error) {
+    useToast().show('warning', `License upgrade failed, please contact support [${error}]`, 6000)
   }
 }
 
 export function getPurchased (force = false) {
-  if (!force && typeof purchased === 'boolean') {
-    return purchased
-  }
-
+  logger.debug('getPurchased', force)
   if (FLAG_DEMO || MODE === 'share-preview') {
     return true
   }
 
-  purchased = !!getLicenseInfo()
+  let token = licenseToken
+  if (typeof token === 'undefined' || force) {
+    token = getLicenseToken()
+  }
 
-  return purchased
+  return !!(token?.isAvailable)
 }
 
-export function showPremium () {
-  getActionHandler('premium.show')()
+export function showPremium (tab?: PremiumTab) {
+  logger.debug('showPremium', tab)
+  upgradeTryCount = 0
+  getActionHandler('premium.show')(tab)
   ga.logEvent('yn_premium_show', { purchased: getPurchased() })
 }
 
-export function getLicenseInfo () {
+export function getLicenseToken () {
+  logger.debug('getLicenseToken')
   try {
-    const licenseStr = getSetting('license')
-    const info = parseLicense(licenseStr!)
-
-    if (info) {
-      ga.setUserProperties({
-        expires: dayjs(info.expires).format('YYYY-MM-DD'),
-        hash: info.hash,
-      })
+    const tokenStr = getSetting('license')
+    if (!tokenStr) {
+      return syncCacheLicenseToken(null)
     }
 
-    return info
+    if (!tokenStr.startsWith(tokenPrefix)) {
+      upgradeV1License(tokenStr)
+      return syncCacheLicenseToken(null)
+    }
+
+    const token = new LicenseToken(
+      tokenStr.slice(tokenPrefix.length),
+      { publicOrPrivateKey: PREMIUM_PUBLIC_KEY }
+    )
+
+    ga.setUserProperties({
+      expires: token.expires.toLocaleDateString(),
+      hash: md5(token.licenseId),
+    })
+
+    return syncCacheLicenseToken(token)
   } catch (error) {
-    console.error(error)
+    logger.error('getLicenseToken', error)
   }
 
-  return null
+  return syncCacheLicenseToken(null)
 }
 
-export async function setLicense (licenseStr: string) {
-  const license = parseLicense(licenseStr)
-
-  const now = await getServerTimestamp()
-  if (!license || now > license.activateExpires) {
-    throw new Error('Invalid License')
+async function cleanLicense () {
+  logger.debug('cleanLicense')
+  // do not clean old license
+  if (!getSetting('license', '').startsWith(tokenPrefix)) {
+    return
   }
 
-  await setSetting('license', licenseStr)
-  refresh()
+  await setSetting('license', '')
 }
+
+async function setLicense (licenseId: string) {
+  logger.debug('setLicense', licenseId)
+  try {
+    if (!licenseId) {
+      return
+    }
+
+    try {
+      const token = await fetchToken(licenseId)
+
+      if (!token.isAvailable) {
+        throw new Error(`Error, license status [${token.status}]`)
+      }
+
+      await setSetting('license', tokenPrefix + token.toString())
+    } catch (error: any) {
+      if (error.message === 'DEVICE_NOT_FOUND') {
+        await cleanLicense()
+      }
+
+      throw error
+    }
+  } catch (error: any) {
+    logger.error('setLicense', error)
+    throw error
+  } finally {
+    getPurchased(true)
+  }
+}
+
+export async function refreshLicense (opts?: { throwError?: boolean }) {
+  logger.debug('refreshLicense', opts)
+  try {
+    const token = getLicenseToken()
+    if (token) {
+      await setLicense(token.licenseId)
+    }
+  } catch (error) {
+    if (opts?.throwError) {
+      throw error
+    } else {
+      logger.error('refreshLicense', error)
+    }
+  }
+}
+
+export async function activateLicense (licenseId: string) {
+  logger.debug('activateLicense', licenseId)
+  await requestApi('addDevice', { licenseId })
+  await setLicense(licenseId)
+}
+
+function checkLicenseStatus () {
+  logger.debug('checkLicenseStatus')
+  const token = getLicenseToken()
+  if (token) {
+    if (token.status === 'stale') {
+      useToast().show('warning', 'License unrecognized, please refresh')
+      showPremium('activation')
+    } else if (tokenIsExpiredSoon(token)) {
+      useToast().show('warning', 'License expires soon, please renew')
+      showPremium('activation')
+    } else if (token.status === 'expired') {
+      useToast().show('warning', 'License expired, please renew')
+      showPremium('activation')
+    }
+  }
+}
+
+async function refreshAndCheckLicenseStatus () {
+  logger.debug('refreshAndCheckLicenseStatus')
+  try {
+    await refreshLicense()
+  } finally {
+    checkLicenseStatus()
+  }
+}
+
+registerHook('STARTUP', refreshAndCheckLicenseStatus)
+setInterval(refreshAndCheckLicenseStatus, 1000 * 60 * 60 * 6) // 6 hours
--- src/share/misc.ts
+++ src/share/misc.ts
@@ -5,6 +5,18 @@ export const DOC_HISTORY_MAX_CONTENT_LENGTH = 102400
 
 export const DEFAULT_EXCLUDE_REGEX = '^node_modules/$|^.git/$|^\\.'
 
+export const PREMIUM_PUBLIC_KEY = `-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAqkiGs7j0xH+RJEHvqZ33
++7nt+tmj5eod4BYbwVWLfoIfAM9dTCUwZkEDEWI2V9W0cYV6eAu4JwKMJqn76jRn
+0S87wtT9H6W2zbbvjK2aia/oCkRilNNOMgV9V6P+ZD0VyDVUSBHWJQk3tOSHf/nS
+GW2hnKqao+loVyuHQQiYp6Iq3ti4Eu+t88LfpxvVZ5uuKmMLo6LbnOMuTFa9mGUE
+R1VuHglANFSi45+45PRHkGlpwjwnlFCTmj137h/djQ//NinJ73CeI3xHD6+Spppy
+259/Ksv+uI/zV39VZWsCrhJkc1pRSUXApKxqXbrMUD2z60Wqz3ps+arn9YeHPR/k
+DQIDAQAB
+-----END PUBLIC KEY-----`
+
+export const API_BASE_URL = 'https://api.yank-note.com'
+
 export function isMarkdownFile (path: string) {
   return path.endsWith(MARKDOWN_FILE_EXT)
 }
