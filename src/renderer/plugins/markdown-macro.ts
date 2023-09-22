269a5d27ec6662c04702a0331444c31b1b3fae9e
purocean <purocean@gmail.com>
2023-09-22 21:51:36 +0800

    chore: fix lint

--- src/renderer/plugins/markdown-macro.ts
+++ src/renderer/plugins/markdown-macro.ts
@@ -1,7 +1,7 @@
 import { escapeRegExp, merge, omit } from 'lodash-es'
 import frontMatter from 'front-matter'
 import type { Plugin } from '@fe/context'
-import type { BuildInSettings, Doc, RenderEnv } from '@fe/types'
+import type { BuildInSettings, Doc } from '@fe/types'
 import type { MenuItem } from '@fe/services/status-bar'
 import { render } from '@fe/services/view'
 import { t } from '@fe/services/i18n'
