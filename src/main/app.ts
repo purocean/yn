ddd5e5c7ccea1324325df84c51297489b23642be
purocean <purocean@gmail.com>
2023-12-17 11:58:19 +0800

    feat: add proxyUrl parameter to `ctx.api.proxyRequest`

--- src/main/app.ts
+++ src/main/app.ts
@@ -15,7 +15,7 @@ import { registerAction } from './action'
 import { registerShortcut } from './shortcut'
 import { initJSONRPCClient, jsonRPCClient } from './jsonrpc'
 import { $t } from './i18n'
-import { getProxyAgent } from './proxy-agent'
+import { getProxyAgent, newProxyAgent } from './proxy-agent'
 import config from './config'
 import { initProxy } from './proxy'
 import { initEnvs } from './envs'
@@ -528,6 +528,7 @@ registerAction('quit', quit)
 registerAction('show-open-dialog', showOpenDialog)
 registerAction('refresh-menus', refreshMenus)
 registerAction('get-proxy-agent', getProxyAgent)
+registerAction('new-proxy-agent', newProxyAgent)
 
 powerMonitor.on('shutdown', quit)
 
