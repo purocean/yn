17096a7dcc2ffa22ba71b3bd3357fd9d62d3bc13
purocean <purocean@gmail.com>
2024-09-10 18:24:28 +0800

    refactor: improve words completion trigger logic

--- src/renderer/plugins/editor-words.ts
+++ src/renderer/plugins/editor-words.ts
@@ -38,6 +38,11 @@ class WordsCompletionProvider implements Monaco.languages.CompletionItemProvider
   }
 
   public async provideCompletionItems (model: Monaco.editor.IModel, position: Monaco.Position): Promise<Monaco.languages.CompletionList | undefined> {
+    const selection = this.ctx.editor.getEditor().getSelection()
+    if (!selection?.isEmpty()) { // only trigger when no selection
+      return
+    }
+
     const currentWord = model.getWordUntilPosition(position)
     const result: Monaco.languages.CompletionItem[] = []
 
