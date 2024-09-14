6e15b1372ce17a8dfa8063c6d0745c7a1e6b2d44
purocean <purocean@gmail.com>
2024-09-14 23:04:49 +0800

    fix: correct rendering issue with PlantUML diagrams

--- src/renderer/plugins/markdown-link.ts
+++ src/renderer/plugins/markdown-link.ts
@@ -199,7 +199,7 @@ function convertLink (state: StateCore) {
       return
     }
 
-    if (/^[a-zA-Z+]+:/.test(attrVal)) { // xxx:
+    if (/^[a-zA-Z+_-]+:/.test(attrVal)) { // xxx:
       return
     }
 
